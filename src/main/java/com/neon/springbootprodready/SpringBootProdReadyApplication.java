0da78d9b400eeef9bef42cecec43bcb96e1d20a8
Ummadi, Mani Kanta Reddy <ManiKantaReddy_Ummadi@comcast.com>
2023-05-31 17:33:14 +0530

    bootstrap project with web & h2 deps

---
+++ src/main/java/com/neon/springbootprodready/SpringBootProdReadyApplication.java
@@ -0,0 +1,14 @@
+package com.neon.springbootprodready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProdReadyApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootProdReadyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/neon/springbootprodready/SpringBootProdReadyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.neon.springbootprodready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProdReadyApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+}
